85ca8371ab4d37c5f88f43015a97f1184fcff2fe
djalil458000 <sahraoui.abdeldjalil@outlook.com>
2026-03-23 07:37:58 +0100

    chore: update config

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -2,8 +2,12 @@ return {
 	"saghen/blink.cmp",
 	version = "*",
 	dependencies = {
-		"rafamadriz/friendly-snippets",
+		{
+			"L3MON4D3/LuaSnip",
+			build = "make install_jsregexp",
+		},
 	},
+
 	opts = {
 		appearance = {
 			nerd_font_variant = "mono",
@@ -11,26 +15,52 @@ return {
 
 		keymap = {
 			preset = "none",
+
 			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
 			["<C-e>"] = { "hide" },
-			["<CR>"] = { "accept", "fallback" },
+
+			["<CR>"] = {
+				function(cmp)
+					if cmp.is_visible() then
+						cmp.accept()
+					else
+						cmp.fallback()
+					end
+				end,
+			},
+
 			["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
 			["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
 			["<C-n>"] = { "select_next", "fallback" },
 			["<C-p>"] = { "select_prev", "fallback" },
+
+			["<C-d>"] = { "scroll_documentation_down", "fallback" },
+			["<C-u>"] = { "scroll_documentation_up", "fallback" },
 		},
 
 		completion = {
-			menu = { border = "rounded" },
-			documentation = { window = { border = "rounded" }, auto_show = true },
-
+			menu = {
+				border = "rounded",
+				scrollbar = true,
+			},
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 200,
+				window = { border = "rounded" },
+			},
 			ghost_text = { enabled = false },
+			list = { selection = { preselect = false } },
 		},
 
-		snippets = { preset = "default" },
+		snippets = {
+			preset = "luasnip",
+			expand = function(snippet)
+				require("luasnip").lsp_expand(snippet.body)
+			end,
+		},
 
 		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
+			default = { "lsp", "snippets", "path", "buffer" },
 		},
 	},
 }
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -7,61 +7,120 @@ return {
 		"saghen/blink.cmp",
 		{
 			"j-hui/fidget.nvim",
-			opts = {
-				notification = {
-					override_vim_notify = true,
-				},
-			},
+			opts = { notification = { override_vim_notify = true } },
 		},
+		{ "L3MON4D3/LuaSnip" },
 	},
+
 	config = function()
 		local lspconfig = require("lspconfig")
-		local capabilities = require("blink.cmp").get_lsp_capabilities()
 
+		-- Capabilities pour blink.cmp
+		local blink_ok, blink = pcall(require, "blink.cmp")
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		if blink_ok then
+			capabilities = blink.get_lsp_capabilities(capabilities)
+		end
+
+		-- Mason Tool Installer : seulement stylua et lua_ls
 		require("mason-tool-installer").setup({
-			ensure_installed = {
-				"lua_ls",
-				"stylua",
-				"ts_ls",
-			},
+			ensure_installed = { "lua_ls", "stylua" },
 		})
+
+		-- Mason LSP config
 		require("mason-lspconfig").setup({
+			ensure_installed = { "lua_ls", "pyright", "yamlls", "bashls", "gopls" },
+			automatic_installation = true,
 			handlers = {
+				-- handler par défaut
 				function(server_name)
 					lspconfig[server_name].setup({ capabilities = capabilities })
 				end,
+
+				-- Lua
 				["lua_ls"] = function()
 					lspconfig.lua_ls.setup({
 						capabilities = capabilities,
-						settings = { Lua = { diagnostics = { globals = { "vim" } } } },
+						settings = {
+							Lua = {
+								diagnostics = { globals = { "vim" } },
+								workspace = {
+									checkThirdParty = false,
+									library = { vim.env.VIMRUNTIME },
+								},
+								telemetry = { enable = false },
+							},
+						},
 					})
 				end,
 
-				["ts_ls"] = function()
-					lspconfig.ts_ls.setup({
+				-- Python
+				["pyright"] = function()
+					lspconfig.pyright.setup({
 						capabilities = capabilities,
-						filetypes = {
-							"javascript",
-							"javascriptreact",
-							"typescript",
-							"typescriptreact",
+						settings = { python = { analysis = { typeCheckingMode = "basic" } } },
+					})
+				end,
+
+				-- YAML
+				["yamlls"] = function()
+					lspconfig.yamlls.setup({
+						capabilities = capabilities,
+						settings = {
+							yaml = { schemas = { ["https://json.schemastore.org/github-workflow.json"] = "*.yml" } },
 						},
+					})
+				end,
+
+				-- Bash
+				["bashls"] = function()
+					lspconfig.bashls.setup({ capabilities = capabilities })
+				end,
+
+				-- Go
+				["gopls"] = function()
+					lspconfig.gopls.setup({
+						capabilities = capabilities,
+						settings = { gopls = { analyses = { unusedparams = true }, staticcheck = true } },
 					})
 				end,
 			},
 		})
 
+		-- Diagnostics
+		vim.diagnostic.config({
+			float = { border = "rounded" },
+			severity_sort = true,
+			signs = true,
+			underline = true,
+			update_in_insert = false,
+		})
+
+		-- Keymaps LSP
 		vim.api.nvim_create_autocmd("LspAttach", {
 			callback = function(event)
 				local map = function(keys, func, desc)
 					vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 				end
+
 				map("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+				map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 				map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+				map("gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+				map("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+
 				map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 				map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+				map("<leader>f", function()
+					vim.lsp.buf.format({ async = true })
+				end, "[F]ormat")
 				map("K", vim.lsp.buf.hover, "Hover Documentation")
 			end,
 		})
+
+		-- LuaSnip setup
+		local luasnip = require("luasnip")
+		luasnip.config.setup({})
+		require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/lua/snippets" })
 	end,
 }
